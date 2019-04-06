ea3a0d9e8d7fe02aa1e2724ea95c2ba4077c33d7
Srinivasu Gannem <srinivasu.gannem@nxp.com>
2019-04-06 14:02:33 +0530

    Initial project setup

---
+++ src/components/home/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './desfire.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Config Transit Bundles
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
